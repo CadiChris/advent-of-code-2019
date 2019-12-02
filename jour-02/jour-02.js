286c17011feeb349c86c188f9dc4b068bf7563d6
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-02 16:44:02 +0100

    Jour 02 - Solution v1

--- jour-02/jour-02.js
+++ jour-02/jour-02.js
@@ -1,3 +1,36 @@
-export const jour02 = {
-  valeur: 988
-};
+export function decode(programme, index = 1) {
+  const [opcode, i1, i2, output] = programme.slice(
+    (index - 1) * 4,
+    index * 4 + 4
+  );
+
+  return {
+    programme,
+    index,
+    instruction: { opcode, inputs: [i1, i2], output }
+  };
+}
+
+export function appliquer({ programme, instruction, index }) {
+  const nouveauProgramme = [...programme];
+  const { opcode, inputs, output } = instruction;
+  const operationParOpcode = {
+    1: () => programme[inputs[0]] + programme[inputs[1]],
+    2: () => programme[inputs[0]] * programme[inputs[1]]
+  };
+
+  nouveauProgramme[output] = operationParOpcode[opcode]();
+
+  return { programme: nouveauProgramme, index, instruction };
+}
+
+export function executer(programme) {
+  const demarrage = decode(programme);
+  let resultat = appliquer(demarrage);
+
+  while (true) {
+    const nextInstruction = decode(resultat.programme, resultat.index + 1);
+    if (nextInstruction.instruction.opcode === 99) return nextInstruction;
+    resultat = appliquer(nextInstruction);
+  }
+}
--- jour-02/jour-02.test.js
+++ jour-02/jour-02.test.js
@@ -1,7 +1,49 @@
-import { jour02 } from "./jour-02";
+import { appliquer, decode, executer } from "./jour-02";
+import { input } from "./input";
 
 describe("Jour 2", () => {
-  it("fonctionne", () => {
-    expect(jour02.valeur).toBe(988);
+  const programme = "1,9,10,3,2,3,11,0,99,30,40,50".split(",").map(Number);
+  it("décode 4 instructions et le reste du programme", () => {
+    expect(decode(programme)).toEqual({
+      programme: [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+      instruction: { opcode: 1, inputs: [9, 10], output: 3 },
+      index: 1
+    });
+  });
+
+  it("applique une instruction au programme", () => {
+    const todo = decode(programme);
+
+    const resultat = appliquer(todo);
+
+    expect(resultat).toEqual({
+      index: 1,
+      instruction: { opcode: 1, inputs: [9, 10], output: 3 },
+      programme: [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    });
+  });
+
+  it("fait avancer l'index du programme", () => {
+    const todo = decode(programme, 2);
+    expect(todo).toEqual({
+      programme: [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+      index: 2,
+      instruction: { opcode: 2, inputs: [3, 11], output: 0 }
+    });
+    expect(decode(programme, 3).instruction.opcode).toEqual(99);
+  });
+
+  it("exécute un programme jusqu'à l'arrêt", () => {
+    const resultat = executer(programme);
+    expect(resultat.index).toBe(3);
+  });
+
+  it("trouve la solution", () => {
+    const programmeAvantIncident = input.split(",").map(Number);
+    programmeAvantIncident[1] = 12;
+    programmeAvantIncident[2] = 2;
+
+    const resultat = executer(programmeAvantIncident);
+    expect(resultat.programme[0]).toBe(3409710);
   });
 });
