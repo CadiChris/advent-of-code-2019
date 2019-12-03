3478e73cc89b8a924fd39860dde62bdfb2e68ff8
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-03 10:50:27 +0100

    Jour 03 - Solution de la partie 01

--- jour-03/jour-03.js
+++ jour-03/jour-03.js
@@ -22,16 +22,24 @@ function deplacement(instruction, pointDeDepart) {
     D: index => ({ ...pointDeDepart, y: pointDeDepart.y - index - 1 })
   };
 
-  const [direction, nombre] = instruction;
+  const direction = instruction.substring(0, 1);
+  const nombre = instruction.substring(1);
   return range(nombre).map(deplacementVers[direction]);
 }
 
-function range(nombre) {
+export function range(nombre) {
   return new Array(Number(nombre)).fill(null).map((_, index) => index);
 }
 
 export function intersections(parcoursA, parcoursB) {
-  return parcoursA.filter(({ x: xA, y: yA }) =>
+  return parcoursA
+    .filter(({ x: xA, y: yA }) =>
       parcoursB.find(({ x: xB, y: yB }) => xA === xB && yA === yB)
-  );
+    )
+    .filter(({ x, y }) => x !== 0 || y !== 0);
+}
+
+export function manhattanMini(points) {
+  const manhattan = ({ x, y }) => Math.abs(x) + Math.abs(y);
+  return Math.min(...points.map(manhattan));
 }
--- jour-03/jour-03.test.js
+++ jour-03/jour-03.test.js
@@ -1,4 +1,5 @@
-import { coordonneesDuParcours, intersections } from "./jour-03";
+import { coordonneesDuParcours, intersections, manhattanMini } from "./jour-03";
+import { inputA, inputB } from "./input";
 
 describe("coordonnées d'un parcours", () => {
   it("va dans une directions", () => {
@@ -44,5 +45,56 @@ describe("intersections de deux parcours", () => {
     ];
 
     expect(intersections(parcoursA, parcoursB)).toEqual([{ x: 1, y: 1 }]);
+  });
+});
+
+describe("distances de manhattan", () => {
+  it("trouve la coordonnée ayant la plus petite distance de Manhattan", () => {
+    expect(
+      manhattanMini([
+        { x: 3, y: 3 },
+        { x: 6, y: 5 }
+      ])
+    ).toBe(6);
+  });
+});
+
+describe("jour 03", () => {
+  it("Example 1", () => {
+    const parcoursA = "R8,U5,L5,D3";
+    const parcoursB = "U7,R6,D4,L4";
+    expect(
+      manhattanMini(
+        intersections(
+          coordonneesDuParcours(parcoursA),
+          coordonneesDuParcours(parcoursB)
+        )
+      )
+    ).toBe(6);
+  });
+
+  it("exemple 2", () => {
+    const parcoursA = "U62,R66,U55,R34,D71,R55,D58,R83";
+    const parcoursB = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+
+    expect(
+      manhattanMini(
+        intersections(
+          coordonneesDuParcours(parcoursA),
+          coordonneesDuParcours(parcoursB)
+        )
+      )
+    ).toBe(159);
+  });
+
+  it("trouve la solution - part 1", () => {
+    expect(
+      manhattanMini(
+        intersections(
+          coordonneesDuParcours(inputA),
+          coordonneesDuParcours(inputB)
+        )
+      )
+    ).toBe(266);
   });
 });
