7b25f70c6c4665fa55e05d77d9766b098403a3f9
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-05 09:22:13 +0100

    Jour 03 - Solution part 02

--- jour-03/jour-03.js
+++ jour-03/jour-03.js
@@ -1,5 +1,5 @@
 export function coordonneesDuParcours(parcours) {
-  const depart = { x: 0, y: 0 };
+  const depart = { x: 0, y: 0, steps: 0 };
 
   const toutesLesInstructions = parcours.split(",");
 
@@ -14,12 +14,13 @@ export function coordonneesDuParcours(parcours) {
   return coordonnees;
 }
 
-function deplacement(instruction, pointDeDepart) {
+function deplacement(instruction, depart) {
+  const { y, x, steps } = depart;
   const deplacementVers = {
-    R: index => ({ ...pointDeDepart, x: pointDeDepart.x + index + 1 }),
-    L: index => ({ ...pointDeDepart, x: pointDeDepart.x - index - 1 }),
-    U: index => ({ ...pointDeDepart, y: pointDeDepart.y + index + 1 }),
-    D: index => ({ ...pointDeDepart, y: pointDeDepart.y - index - 1 })
+    R: i => ({ ...depart, steps: steps + i + 1, x: x + i + 1 }),
+    L: i => ({ ...depart, steps: steps + i + 1, x: x - i - 1 }),
+    U: i => ({ ...depart, steps: steps + i + 1, y: y + i + 1 }),
+    D: i => ({ ...depart, steps: steps + i + 1, y: y - i - 1 })
   };
 
   const direction = instruction.substring(0, 1);
@@ -36,15 +37,22 @@ export function intersections(parcoursA, parcoursB) {
 
   const parcoursBMap = new Map();
   parcoursB.map(coord => parcoursBMap.set(cle(coord), coord));
-
   const estDansParcoursB = coord => parcoursBMap.has(cle(coord));
 
   return parcoursA
     .filter(estDansParcoursB)
-    .filter(({ x, y }) => x !== 0 || y !== 0);
+    .filter(({ x, y }) => x !== 0 || y !== 0)
+    .map(a => ({
+      ...a,
+      steps: a.steps + parcoursBMap.get(cle(a)).steps
+    }));
 }
 
 export function manhattanMini(points) {
   const manhattan = ({ x, y }) => Math.abs(x) + Math.abs(y);
   return Math.min(...points.map(manhattan));
+}
+
+export function stepsMini(coordonnees) {
+  return Math.min(...coordonnees.map(c => c.steps));
 }
--- jour-03/jour-03.test.js
+++ jour-03/jour-03.test.js
@@ -1,50 +1,57 @@
-import { coordonneesDuParcours, intersections, manhattanMini } from "./jour-03";
+import {
+  coordonneesDuParcours,
+  intersections,
+  manhattanMini,
+  stepsMini
+} from "./jour-03";
 import { inputA, inputB } from "./input";
 
 describe("coordonnées d'un parcours", () => {
   it("va dans une directions", () => {
     expect(coordonneesDuParcours("R1")).toEqual([
-      { x: 0, y: 0 },
-      { x: 1, y: 0 }
+      { x: 0, y: 0, steps: 0 },
+      { x: 1, y: 0, steps: 1 }
     ]);
 
     expect(coordonneesDuParcours("R3")).toEqual([
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 3, y: 0 }
+      { x: 0, y: 0, steps: 0 },
+      { x: 1, y: 0, steps: 1 },
+      { x: 2, y: 0, steps: 2 },
+      { x: 3, y: 0, steps: 3 }
     ]);
   });
 
   it("enchaîne deux directions", () => {
     expect(coordonneesDuParcours("R2,R1")).toEqual([
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 3, y: 0 }
+      { x: 0, y: 0, steps: 0 },
+      { x: 1, y: 0, steps: 1 },
+      { x: 2, y: 0, steps: 2 },
+      { x: 3, y: 0, steps: 3 }
     ]);
   });
 
   it("connaît toutes les directions", () => {
     expect(coordonneesDuParcours("R1,U1,L1,D1")).toEqual([
-      { x: 0, y: 0 },
-      { x: 1, y: 0 },
-      { x: 1, y: 1 },
-      { x: 0, y: 1 },
-      { x: 0, y: 0 }
+      { x: 0, y: 0, steps: 0 },
+      { x: 1, y: 0, steps: 1 },
+      { x: 1, y: 1, steps: 2 },
+      { x: 0, y: 1, steps: 3 },
+      { x: 0, y: 0, steps: 4 }
     ]);
   });
 });
 
 describe("intersections de deux parcours", () => {
-  it("trouve une intersection", () => {
-    const parcoursA = [{ x: 1, y: 1 }];
+  it("trouve une intersection et somme ses steps", () => {
+    const parcoursA = [{ x: 1, y: 1, steps: 3 }];
     const parcoursB = [
-      { x: 1, y: 1 },
+      { x: 1, y: 1, steps: 4 },
       { x: 2, y: 1 }
     ];
 
-    expect(intersections(parcoursA, parcoursB)).toEqual([{ x: 1, y: 1 }]);
+    expect(intersections(parcoursA, parcoursB)).toEqual([
+      { x: 1, y: 1, steps: 7 }
+    ]);
   });
 });
 
@@ -59,10 +66,17 @@ describe("distances de manhattan", () => {
   });
 });
 
+describe("steps mini", () => {
+  it("trouve la coordonnées avec le plus petit steps", () => {
+    expect(stepsMini([{ steps: 5 }, { steps: 1 }])).toEqual(1);
+  });
+});
+
 describe("jour 03", () => {
   it("Example 1", () => {
     const parcoursA = "R8,U5,L5,D3";
     const parcoursB = "U7,R6,D4,L4";
+
     expect(
       manhattanMini(
         intersections(
@@ -77,24 +91,22 @@ describe("jour 03", () => {
     const parcoursA = "U62,R66,U55,R34,D71,R55,D58,R83";
     const parcoursB = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
 
-    expect(
-      manhattanMini(
-        intersections(
-          coordonneesDuParcours(parcoursA),
-          coordonneesDuParcours(parcoursB)
-        )
-      )
-    ).toBe(159);
+    const X = intersections(
+      coordonneesDuParcours(parcoursA),
+      coordonneesDuParcours(parcoursB)
+    );
+
+    expect(manhattanMini(X)).toBe(159);
+    expect(stepsMini(X)).toBe(610);
   });
 
-  it("trouve la solution - part 1", () => {
-    expect(
-      manhattanMini(
-        intersections(
-          coordonneesDuParcours(inputA),
-          coordonneesDuParcours(inputB)
-        )
-      )
-    ).toBe(266);
+  it("trouve la solution", () => {
+    const X = intersections(
+      coordonneesDuParcours(inputA),
+      coordonneesDuParcours(inputB)
+    );
+
+    expect(manhattanMini(X)).toBe(266);
+    expect(stepsMini(X)).toBe(19242);
   });
 });
