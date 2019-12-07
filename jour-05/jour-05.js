d93cd128d8e11ba948ee9ae63074d4d9d2c37d3c
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-07 11:06:09 +0100

    Jour 05 - Refactor la création des paramètres

--- jour-05/jour-05.js
+++ jour-05/jour-05.js
@@ -1,60 +1,84 @@
-const OP_CODES = {
-  ADD: 1,
-  MULTIPLY: 2,
-  HALT: 99
-};
+export const OP_CODES = { ADD: 1, MULTIPLY: 2, HALT: 99 };
 
-const PositionParameter = adresse => ({
-  value: programme => programme[adresse]
+export const PositionParameter = adresseDuParametre => ({
+  value: programme => programme[programme[adresseDuParametre]]
 });
-const ImmediateParameter = adresse => ({
-  value: () => adresse
+
+export const ImmediateParameter = adresseDuParametre => ({
+  value: programme => programme[adresseDuParametre]
 });
+
+export const ADRESSE_DEPART = 0;
+export function executer(programme) {
+  let instruction = getInstruction(programme, ADRESSE_DEPART);
+  while (instruction.opcode !== OP_CODES.HALT) {
+    programme = appliquer(instruction, programme);
+    instruction = getInstruction(programme, instruction.nextAdresse);
+  }
+  return programme;
+}
 
 export function getInstruction(programme, adresse) {
-  if (programme[adresse] === OP_CODES.HALT) return { opcode: OP_CODES.HALT };
+  const opcode = getOpcode(programme[adresse]);
+  if (opcode === OP_CODES.HALT) return { opcode: OP_CODES.HALT };
 
-  const [opcode, p1, p2, p3] = programme.slice(adresse, adresse + 4);
-
+  const parametres = getParametres(programme, adresse);
+  const tailleOpcode = 1;
   return {
     opcode,
-    parameters: [
-      PositionParameter(p1),
-      PositionParameter(p2),
-      ImmediateParameter(p3)
-    ],
-    prochaine: adresse + 4
+    parametres,
+    nextAdresse: adresse + tailleOpcode + parametres.length
   };
+}
+
+export function getOpcode(valeur) {
+  const v = String(valeur);
+  return Number(v.substring(v.length - 2, v.length));
+}
+
+export function getParametres(programme, adresse) {
+  const opcodeCourant = programme[adresse];
+  return getModesDesParametres(opcodeCourant).map((mode, index) => {
+    const adresseDuParametre = adresse + index + 1;
+    return mode(adresseDuParametre).value(programme);
+  });
+}
+
+export function getModesDesParametres(opcodeRiche) {
+  const parametreParMode = {
+    0: PositionParameter,
+    1: ImmediateParameter
+  };
+  const jusqua5 = 5 - String(opcodeRiche).length;
+  const instructionSur5 = `${"0".repeat(jusqua5)}${opcodeRiche}`;
+
+  const [modeP3, modeP2, modeP1] = instructionSur5;
+
+  return [
+    parametreParMode[modeP1],
+    parametreParMode[modeP2],
+    ImmediateParameter
+  ];
 }
 
 export function appliquer(instruction, programme) {
   const {
     opcode,
-    parameters: [p1, p2, p3]
+    parametres: [p1, p2, p3]
   } = instruction;
 
   const appliquerOpcode = {
     [OP_CODES.ADD]: () => {
-      const next = [...programme];
-      next[p3.value(programme)] = p1.value(programme) + p2.value(programme);
-      return next;
+      const resultat = [...programme];
+      resultat[p3] = p1 + p2;
+      return resultat;
     },
     [OP_CODES.MULTIPLY]: () => {
-      const next = [...programme];
-      next[p3.value(programme)] = p1.value(programme) * p2.value(programme);
-      return next;
+      const resultat = [...programme];
+      resultat[p3] = p1 * p2;
+      return resultat;
     }
   };
 
   return appliquerOpcode[opcode]();
-}
-
-export const depart = 0;
-export function executer(programme) {
-  let instruction = getInstruction(programme, depart);
-  while (instruction.opcode !== OP_CODES.HALT) {
-    programme = appliquer(instruction, programme);
-    instruction = getInstruction(programme, instruction.prochaine);
-  }
-  return programme;
 }
--- jour-05/jour-05.test.js
+++ jour-05/jour-05.test.js
@@ -1,20 +1,54 @@
-import { appliquer, depart, executer, getInstruction } from "./jour-05";
+import {
+  ADRESSE_DEPART,
+  appliquer,
+  executer,
+  getInstruction,
+  getModesDesParametres,
+  getOpcode,
+  getParametres,
+  ImmediateParameter,
+  OP_CODES,
+  PositionParameter
+} from "./jour-05";
 
 describe("Jour 5", () => {
   const programme = toMemory("1,9,10,3,2,3,11,0,99,30,40,50");
 
   it("décode une instruction", () => {
-    expect(getInstruction(programme, depart)).toMatchObject({
-      opcode: 1,
-      prochaine: depart + 4
+    expect(getInstruction(programme, ADRESSE_DEPART)).toMatchObject({
+      opcode: OP_CODES.ADD,
+      nextAdresse: ADRESSE_DEPART + 4
+    });
+  });
+
+  it("décode le opcode d'une instruction", () => {
+    expect(getOpcode(1101)).toBe(OP_CODES.ADD);
+    expect(getOpcode(1)).toBe(OP_CODES.ADD);
+    expect(getOpcode(99)).toBe(OP_CODES.HALT);
+  });
+
+  describe("paramètres d'instruction", () => {
+    it("donne les modes des paramètres", () => {
+      const valeur = 1002;
+      expect(getModesDesParametres(valeur)).toEqual([
+        PositionParameter,
+        ImmediateParameter,
+        ImmediateParameter
+      ]);
+    });
+
+    it("crée les paramètres d'une instruction", () => {
+      const programme = [1002, 4, 3, 4, 33];
+      const parametres = getParametres(programme, ADRESSE_DEPART);
+      expect(parametres[0]).toBe(33);
+      expect(parametres[1]).toBe(3);
+      expect(parametres[2]).toBe(4);
     });
   });
 
   it("applique une instruction au programme", () => {
-    const instruction = getInstruction(programme, depart);
-
+    const instruction = getInstruction(programme, ADRESSE_DEPART);
     const resultat = appliquer(instruction, programme);
-
     expect(resultat).toEqual([1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
   });
 
@@ -25,6 +59,11 @@ describe("Jour 5", () => {
   it("exécute jusqu'à la fin - 02", () => {
     const resultat = executer(toMemory("1,1,1,4,99,5,6,0,99"));
     expect(resultat).toEqual([30, 1, 1, 4, 2, 5, 6, 0, 99]);
+  });
+
+  it("exécute jusqu'à la fin", () => {
+    const resultat = executer(toMemory("1101,100,-1,4,0"));
+    expect(resultat).toEqual([1101, 100, -1, 4, 99]);
   });
 });
 
