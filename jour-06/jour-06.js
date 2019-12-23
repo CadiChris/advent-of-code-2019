86d68f02a0bc8cf36345b41be5386b78cc97f837
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-23 01:37:39 +0100

    Jour 06 - Part 02

--- jour-06/jour-06.js
+++ jour-06/jour-06.js
@@ -1,4 +1,4 @@
-export function enArbre(orbites) {
+export function enMap(orbites) {
   const laMap = new Map();
 
   for (const o of orbites) {
@@ -14,21 +14,29 @@ export function enArbre(orbites) {
     laMap.get(left).addChild(laMap.get(right));
   }
 
+  return laMap;
+}
+
+export function getCom(orbites) {
+  const laMap = enMap(orbites);
+
   return laMap.get("COM");
 }
 
 class Arbre {
   constructor() {
     this.children = [];
+    this.parent = null;
   }
 
   addChild(child) {
     this.children.push(child);
+    child.parent = this;
   }
 }
 
 export function toutesLesDistances(orbites) {
-  const arbre = enArbre(orbites);
+  const arbre = getCom(orbites);
   return distanceRec(arbre, 0);
 }
 
@@ -38,4 +46,26 @@ function distanceRec(unArbre, uneDistance) {
     accumulateur += distanceRec(child, uneDistance + 1);
   }
   return accumulateur;
+}
+
+export function parentsDe(unArbre) {
+  let parents = [];
+  let leParent = unArbre.parent;
+  while (leParent) {
+    parents.push(leParent);
+    leParent = leParent.parent;
+  }
+  return parents;
+}
+
+export function distancePrefixe(a, b) {
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 && j >= 0 && a[i] === b[j]) {
+    i--;
+    j--;
+  }
+
+  return i + j + 2;
 }
--- jour-06/jour-06.test.js
+++ jour-06/jour-06.test.js
@@ -1,4 +1,10 @@
-import { enArbre, toutesLesDistances } from "./jour-06";
+import {
+  distancePrefixe,
+  enMap,
+  getCom,
+  parentsDe,
+  toutesLesDistances
+} from "./jour-06";
 import { inputJ6 } from "./input";
 
 describe("Jour 06 - part 01", () => {
@@ -22,7 +28,7 @@ describe("Jour 06 - part 01", () => {
 
   it("crée un arbre et son child", () => {
     const orbites = ["COM)B", "COM)C"];
-    const com = enArbre(orbites);
+    const com = getCom(orbites);
     expect(com.children.length).toEqual(2);
     const b = com.children[0];
     expect(b.children.length).toEqual(0);
@@ -30,5 +36,40 @@ describe("Jour 06 - part 01", () => {
 
   it("trouve la solution", () => {
     expect(toutesLesDistances(inputJ6)).toBe(142915);
+  });
+});
+
+describe("Jour 06 - part 02", () => {
+  it("permet d'accéder directement à un noeud", () => {
+    const orbites = ["COM)B", "COM)C"];
+    const laMap = enMap(orbites);
+    expect(laMap.get("C").children.length).toBe(0);
+  });
+
+  it("stocke le parent", () => {
+    const orbites = ["COM)B", "COM)C"];
+    const laMap = enMap(orbites);
+    expect(laMap.get("C").parent).toBe(laMap.get("COM"));
+  });
+
+  it("donne tous ses parents", () => {
+    const orbites = ["COM)B", "COM)C"];
+    const laMap = enMap(orbites);
+    const lesParentsDeB = parentsDe(laMap.get("B"));
+    expect(lesParentsDeB).toEqual([laMap.get("COM")]);
+  });
+
+  it("trouve la distance préfixe", () => {
+    const distance = distancePrefixe(["A", "B"], ["B"]);
+    expect(distance).toBe(1);
+  });
+
+  it("trouve la solution", () => {
+    const laMap = enMap(inputJ6);
+    const entreYouEtSan = distancePrefixe(
+      parentsDe(laMap.get("YOU")),
+      parentsDe(laMap.get("SAN"))
+    );
+    expect(entreYouEtSan).toBe(283);
   });
 });
