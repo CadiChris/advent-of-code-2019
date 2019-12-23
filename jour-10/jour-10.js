503347034d308e4131a9528067a5d10bd9e19520
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-12-23 22:24:01 +0100

    Jour 10 - Trouve la solution part 01

--- jour-10/jour-10.js
+++ jour-10/jour-10.js
@@ -1,3 +1,16 @@
+export function stationOrbitale(map) {
+  let station = { x: 0, y: 0, detections: 0 };
+  for (let x = 0; x < map[0].length; x++) {
+    for (let y = 0; y < map.length; y++) {
+      if (!estUnAsteroide(x, y, map)) continue;
+
+      const detections = asteroide(x, y).detecteDans(map);
+      if (detections > station.detections) station = { x, y, detections };
+    }
+  }
+  return station;
+}
+
 export function asteroide(x, y) {
   return {
     x,
@@ -39,15 +52,16 @@ function leMemeX(xA, xB, yA, yB, map) {
 }
 
 function pasLeMemeX(xA, xB, yA, yB, map) {
-  const alpha = (yB - yA) / (xB - xA);
-  const beta = (yA * xB - yB * xA) / (xB - xA);
+  const alpha = yB - yA;
+  const beta = yA * xB - yB * xA;
 
   let direction = xA < xB ? 1 : -1;
 
   for (let x = xA + direction; x !== xB; x += direction) {
-    const y = alpha * x + beta;
-    const collision = Number.isInteger(y);
-    if (collision && estUnAsteroide(x, y, map)) return false;
+    const yFoisXbMoinsXa = alpha * x + beta;
+    const collision = yFoisXbMoinsXa % (xB - xA) === 0;
+    if (collision && estUnAsteroide(x, yFoisXbMoinsXa / (xB - xA), map))
+      return false;
   }
   return true;
 }
--- jour-10/jour-10.test.js
+++ jour-10/jour-10.test.js
@@ -1,4 +1,5 @@
-import { asteroide } from "./jour-10";
+import { asteroide, stationOrbitale } from "./jour-10";
+import { inputJ10 } from "./input";
 
 describe("Jour 10 - part 01", () => {
   const map = [".#..#", ".....", "#####", "....#", "...##"];
@@ -12,5 +13,61 @@ describe("Jour 10 - part 01", () => {
   it("trouve le nombre d'asteroides visibles par un asteroide", () => {
     expect(asteroide(3, 4).detecteDans(map)).toBe(8);
     expect(asteroide(4, 2).detecteDans(map)).toBe(5);
+  });
+
+  it("trouve où placer la station orbitale", () => {
+    expect(stationOrbitale(map)).toEqual({ x: 3, y: 4, detections: 8 });
+  });
+
+  it("fait l'exemple plus grand", () => {
+    const map = [
+      "......#.#.",
+      "#..#.#....",
+      "..#######.",
+      ".#.#.###..",
+      ".#..#.....",
+      "..#....#.#",
+      "#..#....#.",
+      ".##.#..###",
+      "##...#..#.",
+      ".#....####"
+    ];
+
+    expect(stationOrbitale(map)).toEqual({ x: 5, y: 8, detections: 33 });
+  });
+
+  it("fait l'exemple plus grand 2", () => {
+    const map = [
+      ".#..##.###...#######",
+      "##.############..##.",
+      ".#.######.########.#",
+      ".###.#######.####.#.",
+      "#####.##.#.##.###.##",
+      "..#####..#.#########",
+      "####################",
+      "#.####....###.#.#.##",
+      "##.#################",
+      "#####.##.###..####..",
+      "..######..##.#######",
+      "####.##.####...##..#",
+      ".#####..#.######.###",
+      "##...#.##########...",
+      "#.##########.#######",
+      ".####.#.###.###.#.##",
+      "....##.##.###..#####",
+      ".#.#.###########.###",
+      "#.#.#.#####.####.###",
+      "###.##.####.##.#..##"
+    ];
+
+    expect(stationOrbitale(map)).toEqual({ x: 11, y: 13, detections: 210 });
+  });
+
+  it("trouve la solution", () => {
+    expect(stationOrbitale(inputJ10)).toEqual({
+      detections: 274,
+      x: 19,
+      y: 14
+    });
   });
 });
